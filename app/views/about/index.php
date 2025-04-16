3345466af1620ef148d5181c866cfb9e182b5d17
Falrafa4 <rafaasad9@gmail.com>
2025-04-16 21:12:33 +0700

    `Refactor: Update init.php, views, templates and add Bootstrap CSS and JS`

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
+<div class="mx-5">
+    <h1 class="mt-4">About Me</h1>
+    <!-- <img src="<?= BASEURL ?>" alt=""> -->
     <p>Halo, nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun. Saya adalah seorang <?= $data['pekerjaan'] ?>.</p>
+</div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,9 @@
-    <h1>Selamat Datang di Website Saya</h1>
+<div class="container mx-5 mt-5">
+    <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5">Selamat Datang di Website Saya</h1>
+            <p class="fs-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repellendus magni ratione laudantium quidem quo, cupiditate, veritatis fugiat sed impedit inventore illum labore! Odio id vel natus, dolore cupiditate rerum?</p>
+            <button class="btn btn-primary">Lets Go!</button>
+        </div>
+    </div>
+</div>
