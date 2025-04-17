13cca9ef602518a80a750a3f824a699812abead3
Falrafa4 <rafaasad9@gmail.com>
2025-04-17 09:30:48 +0700

    Update views and picture profile in about page

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,5 +1,5 @@
 <div class="mx-5">
     <h1 class="mt-4">About Me</h1>
-    <!-- <img src="<?= BASEURL ?>" alt=""> -->
-    <p>Halo, nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun. Saya adalah seorang <?= $data['pekerjaan'] ?>.</p>
+    <img src="<?= BASEURL ?>/img/blank.png" alt="Orang" width="200" class="rounded-circle shadow mb-3">
+    <p class="fs-5">Halo, nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun. Saya adalah seorang <?= $data['pekerjaan'] ?>.</p>
 </div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,9 +1,9 @@
 <div class="container mx-5 mt-5">
-    <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+    <div class="p-5 mb-4  rounded-3">
         <div class="container-fluid py-5">
-            <h1 class="display-5">Selamat Datang di Website Saya</h1>
-            <p class="fs-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repellendus magni ratione laudantium quidem quo, cupiditate, veritatis fugiat sed impedit inventore illum labore! Odio id vel natus, dolore cupiditate rerum?</p>
-            <button class="btn btn-primary">Lets Go!</button>
+            <h1 class="display-5 fw-light mb-3">Selamat Datang di Website Saya</h1>
+            <p class="fs-5">Website ini digunakan sebagai penunjang untuk belajar MVC (Model View Controller) pada PHP sebagai syarat kelengkapan untuk mempelajari Framework Laravel.</p>
+            <a class="btn btn-primary px-4" href="<?= BASEURL ?>/about/">About Me</a>
         </div>
     </div>
 </div>
